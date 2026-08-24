7db1d25e32554461d449152267874898955407e1
vivek092002 <kumar.vivek092002@gmail.com>
2026-08-24 22:00:29 +0530

    various queries

---
+++ src/main/java/org/vivek/module3/repositories/ProductRepositories.java
@@ -0,0 +1,20 @@
+package org.vivek.module3.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.vivek.module3.enitites.Product;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepositories extends JpaRepository<Product, Long> {
+    List<Product>  findByTitle(String title);
+    List<Product> findByCreatedAtAfter(LocalDateTime after);
+
+    @Query("select e from product e where e.title = ?1 and e.price=?2" )
+    Optional<Product> findByTitleAndPrice(String title, BigDecimal price);
+}
--- src/test/java/org/vivek/module3/Module3ApplicationTests.java
+++ src/test/java/org/vivek/module3/Module3ApplicationTests.java
@@ -1,13 +1,42 @@
 package org.vivek.module3;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.vivek.module3.enitites.Product;
+import org.vivek.module3.repositories.ProductRepositories;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
 class Module3ApplicationTests {
 
+    @Autowired
+    ProductRepositories productRepositories;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testRepository(){
+        Product product = Product.builder()
+                .sku("nestle234")
+                .title("Nestle Chocolate")
+                .price(BigDecimal.valueOf(123.45))
+                .quantity(12)
+                .build();
+        Product savedProductEntity = productRepositories.save(product);
+        System.out.println(savedProductEntity);
+    }
+
+    @Test
+    void getRepository() {
+//        List<Product> entities = productRepositories.findAll();
+        List<Product> entities = productRepositories.findByCreatedAtAfter(LocalDateTime.of(2024, 1, 1 , 0, 0));
+        System.out.println(entities);
     }
 
 }
